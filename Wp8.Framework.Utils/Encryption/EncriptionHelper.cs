d8208cae8f50a13885d12d6471d0df7e48b7e25d
migorepo <learningitems@gmail.com>
2013-11-16 08:22:24 +0530

    Added working / Updated SharpZipLib for wp8

---
+++ Wp8.Framework.Utils/Encryption/EncriptionHelper.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+// http://janhannemann.wordpress.com/2013/11/10/simple-encryption-for-windows-winrt-and-windows-phone/
+
+namespace Wp8.Framework.Utils.Encryption
+{
+    public static class EncryptionHelper
+    {
+
+        public static byte[] Encrypt(string dataToEncrypt, string password, string salt)
+        {
+            AesManaged aes = null;
+            MemoryStream memoryStream = null;
+            CryptoStream cryptoStream = null;
+
+            try
+            {
+                //Generate a Key based on a Password, Salt and HMACSHA1 pseudo-random number generator 
+                var rfc2898 = new Rfc2898DeriveBytes(password, Encoding.Unicode.GetBytes(salt));
+
+                //Create AES algorithm with 256 bit key and 128-bit block size 
+                aes = new AesManaged();
+                aes.Key = rfc2898.GetBytes(aes.KeySize / 8);
+                rfc2898.Reset(); //needed for WinRT compatibility
+                aes.IV = rfc2898.GetBytes(aes.BlockSize / 8);
+
+                //Create Memory and Crypto Streams 
+                memoryStream = new MemoryStream();
+                cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+                //Encrypt Data 
+                byte[] data = Encoding.Unicode.GetBytes(dataToEncrypt);
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+
+                //Return encrypted data 
+                return memoryStream.ToArray();
+
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                if (cryptoStream != null)
+                    cryptoStream.Close();
+
+                if (memoryStream != null)
+                    memoryStream.Close();
+
+                if (aes != null)
+                    aes.Clear();
+
+            }
+        }
+
+        public static string Decrypt(byte[] dataToDecrypt, string password, string salt)
+        {
+            AesManaged aes = null;
+            MemoryStream memoryStream = null;
+            CryptoStream cryptoStream = null;
+            string decryptedText = "";
+            try
+            {
+                //Generate a Key based on a Password, Salt and HMACSHA1 pseudo-random number generator 
+                var rfc2898 = new Rfc2898DeriveBytes(password, Encoding.Unicode.GetBytes(salt));
+
+                //Create AES algorithm with 256 bit key and 128-bit block size 
+                aes = new AesManaged();
+                aes.Key = rfc2898.GetBytes(aes.KeySize / 8);
+                rfc2898.Reset(); //neede to be WinRT compatible
+                aes.IV = rfc2898.GetBytes(aes.BlockSize / 8);
+
+                //Create Memory and Crypto Streams 
+                memoryStream = new MemoryStream();
+                cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+                //Decrypt Data 
+                cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                cryptoStream.FlushFinalBlock();
+
+                //Return Decrypted String 
+                byte[] decryptBytes = memoryStream.ToArray();
+                decryptedText = Encoding.Unicode.GetString(decryptBytes, 0, decryptBytes.Length);
+            }
+            catch (Exception eDecrypt)
+            {
+                Debug.WriteLine("Error Caught while decription {0}", eDecrypt);
+            }
+            finally
+            {
+                if (cryptoStream != null)
+                    cryptoStream.Close();
+
+                if (memoryStream != null)
+                    memoryStream.Close();
+
+                if (aes != null)
+                    aes.Clear();
+            }
+            return decryptedText;
+        }
+
+    }
+}
---
+++ Wp8.Framework.Utils/Utils/AsyncHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wp8.Framework.Utils.Utils
+{
+    #region from http://blogs.msdn.com/b/pfxteam/archive/2012/02/12/10266988.aspx Building Async Coordination Primitives
+    public class AsyncSemaphore
+    {
+        private readonly static Task SCompleted = Task.FromResult(true);
+        private readonly Queue<TaskCompletionSource<bool>> _mWaiters = new Queue<TaskCompletionSource<bool>>();
+        private int _mCurrentCount;
+
+
+        public AsyncSemaphore(int initialCount)
+        {
+            if (initialCount < 0) throw new ArgumentOutOfRangeException("initialCount");
+            _mCurrentCount = initialCount;
+        }
+
+
+
+        public Task WaitAsync()
+        {
+            lock (_mWaiters)
+            {
+                if (_mCurrentCount > 0)
+                {
+                    --_mCurrentCount;
+                    return SCompleted;
+                }
+                var waiter = new TaskCompletionSource<bool>();
+                _mWaiters.Enqueue(waiter);
+                return waiter.Task;
+            }
+        }
+        public void Release()
+        {
+            TaskCompletionSource<bool> toRelease = null;
+            lock (_mWaiters)
+            {
+                if (_mWaiters.Count > 0)
+                    toRelease = _mWaiters.Dequeue();
+                else
+                    ++_mCurrentCount;
+            }
+            if (toRelease != null)
+                toRelease.SetResult(true);
+        }
+    }
+    public class AsyncLock
+    {
+
+
+        public struct Releaser : IDisposable
+        {
+            private readonly AsyncLock _mToRelease;
+
+            internal Releaser(AsyncLock toRelease) { _mToRelease = toRelease; }
+
+            public void Dispose()
+            {
+                if (_mToRelease != null)
+                    _mToRelease._mSemaphore.Release();
+            }
+        }
+
+        private readonly AsyncSemaphore _mSemaphore;
+        private readonly Task<Releaser> _mReleaser;
+
+
+        public AsyncLock()
+        {
+            _mSemaphore = new AsyncSemaphore(1);
+            _mReleaser = Task.FromResult(new Releaser(this));
+        }
+        public Task<Releaser> LockAsync()
+        {
+            var wait = _mSemaphore.WaitAsync();
+            return wait.IsCompleted ?
+                _mReleaser :
+                wait.ContinueWith((_, state) => new Releaser((AsyncLock)state),
+                    this, CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+        }
+
+
+    }
+    #endregion
+}
+
